b42ef12d6e268330f91d9434b1035b60d6945844
l00my <loomy91@gmail.com>
2019-11-14 13:43:40 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFpsSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ConsoleFpsSharp
+ C# implementation of the FPS made by One Lone Coder (https://www.youtube.com/watch?v=xW8skO7MFYw)
